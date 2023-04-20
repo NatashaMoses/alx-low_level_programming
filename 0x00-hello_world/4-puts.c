dbced572e43e2f0382794d82c261e0d587523870
Natasha Moses <natmos910@gmail.com>
2023-04-20 16:05:13 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+/**
+ * this program prints a line using puts function
+ * return(0) means success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
