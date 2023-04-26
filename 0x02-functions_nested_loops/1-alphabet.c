fd238d1ec6c82e7b1986196e26ebc101418ce42b
Natasha Moses <natmos910@gmail.com>
2023-04-25 18:10:04 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - program print lowercase alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
