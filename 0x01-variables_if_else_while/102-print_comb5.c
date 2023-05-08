ba7e9a64d5bef9f6a8bdfe7c002730775eec7d89
Natasha Moses <natmos910@gmail.com>
2023-05-08 12:41:24 -0700

    advanced

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Program that print all possible combo of 2digits number
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n < 100; n++)
+	{
+		for (m = 0; m < 100; m++)
+		{
+			if (n < m)
+			{
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
+				putchar(' ');
+				putchar((m / 10) + 48);
+				putchar((m % 10) + 48);
+				if (n != 98 || m != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	return (0);
+}
